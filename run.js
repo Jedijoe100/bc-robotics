4b7fdc3257ce5c8c57f80e84e4a45d40792808d1
DESKTOP-3LEPBII\Joe Kent <thisisjedijoe@gmail.com>
2018-06-16 08:17:26 +1200

    work for the saturaday

--- run.js
+++ run.js
@@ -7,48 +7,12 @@ express()
     .set('views', path.join(__dirname, 'views'))
     .set('view engine', 'ejs')
     .get('/', (req, res) => res.render('pages/index'))
-    .get('/people', (req, res) => res.render('pages/people'))
+	.get('/sponsors', (req, res) => res.render('pages/sponsors'))
+	.get('/junior-secondary', (req, res) => res.render('pages/jSecondary'))
+	.get('/primary', (req, res) => res.render('pages/primary'))
+	.get('/senior-secondary', (req, res) => res.render('pages/sSecondary'))
     .get('/club', (req, res) => res.render('pages/club'))
-    .get('/robots', (req, res) => res.render('pages/robots'))
     .get('/articles', (req, res) => res.contentType('text/xml').render('pages/articles'))
     .get('/images', (req, res) => res.contentType('text/xml').render('pages/images'))
     //.get('/articles', (req, res) => res.render('pages/articles'))
     .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
-
-/*//node D:\Documents\website\run.js
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
-var path = require('path');
-var path1 = "";
-http.createServer(function (req, res) {
-	var q = url.parse(req.url, true);
-	var filename = q.pathname;
-	var fileloc = path1 + filename;
-	var filetype = path.extname(filename);
-	fs.readFile(fileloc, function(err, data) {
-		var content = data;
-		if (err) {
-			res.writeHead(404, {'Content-Type': 'text/html'});
-			return res.end("404 Not Found");
-		}
-
-		switch (filetype){
-			case '.html':
-			    res.writeHead(200, {'Content-Type': 'text/html'});
-			    break;
-			case '.css':
-			    res.writeHead(200, {'Content-Type': 'text/css'});
-			    break;
-			case '.xml':
-			    res.writeHead(200, {'Content-Type': 'text/xml'});
-			    break;
-            case '.png':
-                res.writeHead(200, {'Content-Type': 'image/png'});
-                break;
-		}
-		res.write(content);
-		return res.end();
-	});
-}).listen(8080);*/
